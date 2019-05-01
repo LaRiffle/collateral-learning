63cd488ffb735e8196b59687010e3a3ed58d950e
LaRiffle <theo.leffyr@gmail.com>
2019-05-01 14:06:05 +0200

    Add cv dataset choice for get data

--- learn/collateral/data.py
+++ learn/collateral/data.py
@@ -11,7 +11,39 @@ import torch.utils.data as utils
 import pickle
 
 
-def load_resistance_data(font1, font2):
+def get_data_loaders(args, font1=None, font2=None, cv=6):
+    torch.manual_seed(1)
+
+    train_dataset, test_dataset = get_datasets(font1, font2, cv)
+
+    train_loader = utils.DataLoader(
+        train_dataset,
+        batch_size=args.batch_size, shuffle=True
+    )
+
+    test_loader = utils.DataLoader(
+        test_dataset,
+        batch_size=args.test_batch_size, shuffle=True
+    )
+    return train_loader, test_loader
+
+
+def get_datasets(font1, font2, cv=6):
+    data = load_resistance_data(font1, font2, cv)
+    train_data, train_target_char, train_target_family, test_data, test_target_char, test_target_family = data
+    # Merge the target datasets
+    train_target = list(zip(train_target_char, train_target_family))
+    test_target = list(zip(test_target_char, test_target_family))
+
+    # We use here the slightly modified version of this function
+    train_dataset = build_tensor_dataset(train_data, train_target)
+    test_dataset = build_tensor_dataset(test_data, test_target)
+
+    return train_dataset, test_dataset
+
+
+def load_resistance_data(font1, font2, cv=6):
+    print('cv=', cv)
 
     base_name = f'dataset/character_dataset_{font1}_{font2}' \
         if font1 is not None and font2 is not None \
@@ -19,19 +51,23 @@ def load_resistance_data(font1, font2):
 
     train_data, train_target_char, train_target_family = [], [], []
 
-    # The training set
-    for i in range(6):
-        with open(f'{base_name}_train{i}.pkl', 'rb') as input_file:
-            training_set = pickle.load(input_file)
-            train_data_i, train_target_char_i, train_target_family_i = training_set
-            train_data += train_data_i
-            train_target_char += train_target_char_i
-            train_target_family += train_target_family_i
+    data_files = [f'{base_name}_train{i}.pkl' for i in range(6)] + [f'{base_name}_test.pkl']
 
-    # The testing set
-    with open(f'{base_name}_test.pkl', 'rb') as input_file:
-        testing_set = pickle.load(input_file)
-        test_data, test_target_char, test_target_family = testing_set
+    assert 0 <= cv <= len(data_files)
+
+    for i, data_file in enumerate(data_files):
+        with open(data_file, 'rb') as input_file:
+            # Build the training set
+            if i != cv:
+                training_set = pickle.load(input_file)
+                train_data_i, train_target_char_i, train_target_family_i = training_set
+                train_data += train_data_i
+                train_target_char += train_target_char_i
+                train_target_family += train_target_family_i
+            # The testing set
+            else:
+                testing_set = pickle.load(input_file)
+                test_data, test_target_char, test_target_family = testing_set
 
     print('Training set', len(train_data), 'items')
     print('Testing set ', len(test_data), 'items')
@@ -41,23 +77,60 @@ def load_resistance_data(font1, font2):
     return data
 
 
-def load_collateral_data(font1, font2, letter):
+def get_collateral_data_loaders(args, font1, font2, letter, cv=6):
+    torch.manual_seed(1)
 
+    train_dataset, test_dataset = get_collateral_datasets(font1, font2, letter, cv)
+
+    train_loader = utils.DataLoader(
+        train_dataset,
+        batch_size=args.batch_size, shuffle=True
+    )
+
+    test_loader = utils.DataLoader(
+        test_dataset,
+        batch_size=args.test_batch_size, shuffle=True
+    )
+    return train_loader, test_loader
+
+
+def get_collateral_datasets(font1, font2, letter, cv=6):
+    data = load_collateral_data(font1, font2, letter, cv)
+    train_data, train_target_char, train_target_family, test_data, test_target_char, test_target_family = data
+    # Merge the target datasets
+    train_target = list(zip(train_target_char, train_target_family))
+    test_target = list(zip(test_target_char, test_target_family))
+
+    # We use here the slightly modified version of this function
+    train_dataset = build_tensor_dataset(train_data, train_target)
+    test_dataset = build_tensor_dataset(test_data, test_target)
+
+    return train_dataset, test_dataset
+
+
+def load_collateral_data(font1, font2, letter, cv=6):
+    print('cv=', cv)
+    base_name = f'dataset/character_dataset'
     train_data, train_target_char, train_target_family = [], [], []
 
-    # The training set
-    for i in range(6):
-        with open(f'dataset/character_dataset_{font1}_{font2}_{letter}_train{i}.pkl', 'rb') as input_file:
-            training_set = pickle.load(input_file)
-            train_data_i, train_target_char_i, train_target_family_i = training_set
-            train_data += train_data_i
-            train_target_char += train_target_char_i
-            train_target_family += train_target_family_i
+    data_files = [f'{base_name}_{font1}_{font2}_{letter}_train{i}.pkl' for i in range(6)] \
+                 + [f'{base_name}_{font1}_{font2}_{letter}_test.pkl']
 
-    # The testing set
-    with open(f'dataset/character_dataset_{font1}_{font2}_{letter}_test.pkl', 'rb') as input_file:
-        testing_set = pickle.load(input_file)
-        test_data, test_target_char, test_target_family = testing_set
+    assert 0 <= cv <= len(data_files)
+
+    for i, data_file in enumerate(data_files):
+        with open(data_file, 'rb') as input_file:
+            # Build the training set
+            if i != cv:
+                training_set = pickle.load(input_file)
+                train_data_i, train_target_char_i, train_target_family_i = training_set
+                train_data += train_data_i
+                train_target_char += train_target_char_i
+                train_target_family += train_target_family_i
+            # The testing set
+            else:
+                testing_set = pickle.load(input_file)
+                test_data, test_target_char, test_target_family = testing_set
 
     print('Training set', len(train_data), 'items')
     print('Testing set ', len(test_data), 'items')
@@ -74,65 +147,3 @@ def build_tensor_dataset(data, target):
     target = torch.LongTensor([[i[0][0], i[1][0]] for i in target])
     tensor_dataset = utils.TensorDataset(normed_data, target)
     return tensor_dataset
-
-
-def get_datasets(font1, font2):
-    data = load_resistance_data(font1, font2)
-    train_data, train_target_char, train_target_family, test_data, test_target_char, test_target_family = data
-    # Merge the target datasets
-    train_target = list(zip(train_target_char, train_target_family))
-    test_target = list(zip(test_target_char, test_target_family))
-
-    # We use here the slightly modified version of this function
-    train_dataset = build_tensor_dataset(train_data, train_target)
-    test_dataset = build_tensor_dataset(test_data, test_target)
-
-    return train_dataset, test_dataset
-
-
-def get_data_loaders(args, font1=None, font2=None):
-    torch.manual_seed(1)
-
-    train_dataset, test_dataset = get_datasets(font1, font2)
-
-    train_loader = utils.DataLoader(
-        train_dataset,
-        batch_size=args.batch_size, shuffle=True
-    )
-
-    test_loader = utils.DataLoader(
-        test_dataset,
-        batch_size=args.test_batch_size, shuffle=True
-    )
-    return train_loader, test_loader
-
-
-def get_collateral_datasets(font1, font2, letter):
-    data = load_collateral_data(font1, font2, letter)
-    train_data, train_target_char, train_target_family, test_data, test_target_char, test_target_family = data
-    # Merge the target datasets
-    train_target = list(zip(train_target_char, train_target_family))
-    test_target = list(zip(test_target_char, test_target_family))
-
-    # We use here the slightly modified version of this function
-    train_dataset = build_tensor_dataset(train_data, train_target)
-    test_dataset = build_tensor_dataset(test_data, test_target)
-
-    return train_dataset, test_dataset
-
-
-def get_collateral_data_loaders(args, font1, font2, letter):
-    torch.manual_seed(1)
-
-    train_dataset, test_dataset = get_collateral_datasets(font1, font2, letter)
-
-    train_loader = utils.DataLoader(
-        train_dataset,
-        batch_size=args.batch_size, shuffle=True
-    )
-
-    test_loader = utils.DataLoader(
-        test_dataset,
-        batch_size=args.test_batch_size, shuffle=True
-    )
-    return train_loader, test_loader
--- learn/collateral_OLD/__init__.py
+++
@@ -1,3 +0,0 @@
-from .test import collateral_test
-from .train import collateral_train
-from .main import collateral_task
